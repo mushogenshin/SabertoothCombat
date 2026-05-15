9c0c9dfb543c6711444d2612c28b22bbb185b189
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-05-15 18:27:19 +0700

    WIP: CombatCharacterBase, with ASC and BasicAttributeSet

---
+++ Combat/Source/Combat/Public/Characters/CombatCharacterBase.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CombatCharacterBase.generated.h"
+
+class UBasicAttributeSet;
+class UAbilitySystemComponent;
+
+UCLASS()
+class COMBAT_API ACombatCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACombatCharacterBase();
+	
+	// Ability System Component
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AbilitySystem")
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AbilitySystem")
+	UBasicAttributeSet* BasicAttributeSet;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
