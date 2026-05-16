d18c47e2faead77520a78606336ddf34ac503ffe
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-05-16 19:29:06 +0700

    AttributeSet virtual methods

--- Combat/Source/Combat/Private/Gameplay/Attributes/BasicAttributeSet.cpp
+++ Combat/Source/Combat/Private/Gameplay/Attributes/BasicAttributeSet.cpp
@@ -10,3 +10,23 @@ UBasicAttributeSet::UBasicAttributeSet()
 	Stamina = 100.f;
 	MaxStamina = 100.f;
 }
+
+void UBasicAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
+
+void UBasicAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+}
+
+void UBasicAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+}
+
+void UBasicAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+}
--- Combat/Source/Combat/Public/Gameplay/Attributes/BasicAttributeSet.h
+++ Combat/Source/Combat/Public/Gameplay/Attributes/BasicAttributeSet.h
@@ -60,4 +60,11 @@ public:
 		GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxStamina, OldValue);
 	}
 
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
 };
