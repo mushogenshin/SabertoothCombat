3f6bfe9e99194b089d0ff3850556a4e378aa12b9
Hoan Nguyen <33324682+mushogenshin@users.noreply.github.com>
2026-06-06 09:42:20 -0500

    WIP: needs EnhancedInput module

--- Combat/Source/Combat/Combat.Build.cs
+++ Combat/Source/Combat/Combat.Build.cs
@@ -8,7 +8,7 @@ public class Combat : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new[] {
 			"GameplayAbilities", "GameplayTasks", "GameplayTags"
